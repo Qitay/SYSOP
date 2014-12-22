78dd5649b1857087bd6186f0fdcabefc30ce8807
Qitay <qitay2@gmail.com>
2014-12-23 00:37:11 +0100

    sd2

--- SYSOP/src/pipes/Pipe.java
+++ SYSOP/src/pipes/Pipe.java
@@ -4,7 +4,19 @@ public class Pipe {
 	
 	public int pipe(int fd)
 	{
-		//tutaj coœ sie bedzie dzia³o
+	/*	sprawdzenie poprawnosci parametru fd;
+   		przydziel nowy i-wezel z urzadzenia do przechowywania laczy
+      	(za pomoca funkcji get_pipe_inode);
+   		znajdz dwie wolne pozycje w tablicy plikow i przydziel pierwsza do
+      	czytania a druga do pisania (za pomoca funkcji get_empty_filp);
+   		znajdz dwie wolne pozycje w tablicy deskryptorow procesu i zainicjuj
+      	je, tak by wskazywaly na odpowiednie pozycje w tablicy plikow, 
+      	podstawiajac od razu odpowiednie wartosci w argumencie fd
+      	(fd[0]= deskryptor do czytania, fd[1]= deskryptor do pisania);
+   		zainicjuj znalezione pozycje w tablicy plikow, by wskazywaly na nowy i-wezel;
+   		ustaw flagi i wskaznik do struktury z operacjami w znalezionych pozycjach
+      	w tablicy plikow odpowiednio do czytania i pisania;
+    */
 		return 0;
 	};
 }
