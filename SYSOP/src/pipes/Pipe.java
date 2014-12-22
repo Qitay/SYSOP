bfbbd48709bab26eb2756f541b42310e41dccb94
Qitay <qitay2@gmail.com>
2014-12-23 00:30:11 +0100

    sd

--- SYSOP/src/pipes/Pipe.java
+++ SYSOP/src/pipes/Pipe.java
@@ -1,19 +1,10 @@
 package pipes;
 
 public class Pipe {
-<<<<<<< HEAD
-	public int pipe(int fd)
-	{
-		return 0;
-	}
-
-=======
 	
 	public int pipe(int fd)
 	{
 		//tutaj coœ sie bedzie dzia³o
 		return 0;
 	};
-	
->>>>>>> branch 'master' of https://github.com/Qitay/SYSOP.git
 }
