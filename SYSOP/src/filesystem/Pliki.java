9b60b17d48cd6df3dfb3897eab314ea1e2332870
Qitay <Qitay@Qitay-PC>
2015-01-03 17:41:09 +0100

    Czesc Bartka, zabieram sie za moje troche

---
+++ SYSOP/src/filesystem/Pliki.java
@@ -0,0 +1,146 @@
+package filesystem;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+
+
+
+///////////////////////////////////////	
+class Iwezel{
+	int typ_pliku;
+	int i_block;
+	boolean[] prawa;
+	String nazwa;
+	
+	//CREATE////////////////////////////////////////////////////
+	public Iwezel(Blok[] dysk){
+		System.out.println("Nazwa pliku:");
+		Scanner scan=new Scanner(System.in);
+		String zapis=scan.nextLine();
+		nazwa=zapis;
+		typ_pliku=1;
+		for(int i=0;i<32;i++)
+			if(dysk[i].zajety==false){
+				i_block=i;
+				dysk[i].zajety=true;
+				break;
+			}
+	}
+
+	//WRITE/////////////////////////////////////////////////////
+	public void write(Blok[] dysk,ArrayList<Iwezel> wezly){
+		System.out.println("Do ktorego pliku chcesz napisac");
+		Scanner scan=new Scanner(System.in);
+		String zapis=scan.nextLine();
+		int nr = Integer.parseInt(zapis);
+		System.out.println("Co chcesz zapisac");
+		zapis=scan.nextLine();
+		write_2(dysk,wezly,zapis,wezly.get(nr).i_block);
+	}
+	
+	public void write_2(Blok[] dysk,ArrayList<Iwezel> wezly,String zapis,int nr){
+		if(zapis.length()<8)dysk[nr].dane=zapis.toCharArray();
+			else{
+				dysk[nr].dane=zapis.substring(0, 8).toCharArray();
+				zapis=zapis.substring(8);
+				for(int i=0;i<32;i++)
+					if(dysk[i].zajety==false){
+						dysk[i].zajety=true;
+						dysk[nr].nr_nast_bloku=i;
+						break;
+					}
+				write_2(dysk,wezly,zapis,dysk[nr].nr_nast_bloku);
+			}
+	}
+	
+	//READ//////////////////////////////////////////////////////
+	public void read(Blok[] dysk,ArrayList<Iwezel> wezly){
+		System.out.println("Ktory plik chcesz odczytac");
+		Scanner scan=new Scanner(System.in);
+		String zapis=scan.nextLine();
+		int nr = Integer.parseInt(zapis);
+		read_2(dysk,wezly,wezly.get(nr).i_block);
+		System.out.println();
+	}
+	
+	public void read_2(Blok[] dysk,ArrayList<Iwezel> wezly,int nr){
+		if(dysk[nr].nr_nast_bloku==0)
+		System.out.print(dysk[nr].dane);
+		else{
+			System.out.print(dysk[nr].dane);
+			nr=dysk[nr].nr_nast_bloku;
+			read_2(dysk,wezly,nr);
+		}
+	}
+	
+	//ZAWARTOSCDYSKU////////////////////////////////////////////
+	public void zawdysk(Blok[] dysk,ArrayList<Iwezel> wezly){
+		for(int i=0;i<32;i++){
+			System.out.print("Blok "+i+":");
+			System.out.println(dysk[i].dane);
+		}
+	}
+	
+	//PLIKI/////////////////////////////////////////////////////
+	public void wyswpliki(Blok[] dysk,ArrayList<Iwezel> wezly){
+		for(int i=0;i<wezly.size();i++){
+			System.out.print("Plik "+i+":");
+			System.out.println(wezly.get(i).nazwa);
+		}
+	}
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////
+class Blok{
+	char[] dane;
+	int nr_nast_bloku;
+	boolean zajety;
+	public Blok() {
+		dane=" ".toCharArray();
+		nr_nast_bloku=0;
+		zajety=false;
+	}
+}	
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+public class Pliki 
+{	
+
+	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int x = 0;
+		
+		ArrayList<Iwezel> wezly= new ArrayList<Iwezel>();
+		Blok[] dysktwardy=new Blok[32];	
+		for(int i=0;i<32;i++)dysktwardy[i]=new Blok();
+
+		
+		while(x!=6){
+			System.out.println("1-stworz plik");
+			System.out.println("2-wyswietl plik");
+			System.out.println("3-zapisz do pliku");
+			System.out.println("4-wyswietl zawartosc dysku");
+			System.out.println("5-wyswietl nazwy plikow");
+			System.out.println("6-wyjdz");
+			System.out.println("Co chcesz zrobic:");
+			String zapis=scan.nextLine();
+			x = Integer.parseInt(zapis);
+			if(x==1){
+				wezly.add(new Iwezel(dysktwardy));
+			}
+			if(x==2){
+				wezly.get(0).read(dysktwardy, wezly);
+			}
+			if(x==3){
+				wezly.get(0).write(dysktwardy, wezly);
+			}
+			if(x==4){
+				wezly.get(0).zawdysk(dysktwardy, wezly);
+			}	
+			if(x==5){
+				wezly.get(0).wyswpliki(dysktwardy, wezly);
+			}
+		}
+	}
+}
--- SYSOP/src/pipes/Pipe.java
+++ SYSOP/src/pipes/Pipe.java
@@ -5,18 +5,47 @@ public class Pipe {
 	public int pipe(int fd)
 	{
 	/*	sprawdzenie poprawnosci parametru fd;
+	  
    		przydziel nowy i-wezel z urzadzenia do przechowywania laczy
       	(za pomoca funkcji get_pipe_inode);
+   		
    		znajdz dwie wolne pozycje w tablicy plikow i przydziel pierwsza do
-      	czytania a druga do pisania (za pomoca funkcji get_empty_filp);
+      	czytania a druga do pisania fd[0] i fd[1];
+      	
    		znajdz dwie wolne pozycje w tablicy deskryptorow procesu i zainicjuj
       	je, tak by wskazywaly na odpowiednie pozycje w tablicy plikow, 
       	podstawiajac od razu odpowiednie wartosci w argumencie fd
       	(fd[0]= deskryptor do czytania, fd[1]= deskryptor do pisania);
+      	
    		zainicjuj znalezione pozycje w tablicy plikow, by wskazywaly na nowy i-wezel;
+   		
    		ustaw flagi i wskaznik do struktury z operacjami w znalezionych pozycjach
       	w tablicy plikow odpowiednio do czytania i pisania;
     */
 		return 0;
 	};
+	
+	public int write(int fd)
+	{
+		return 0;
+	}
+	
+	public int read(int fd)
+	{
+		return 0;
+	}
+	
+	public int close(int fd)
+	{
+		return 0;
+	}
+	
+	
+	// MAIN MAIN MAIN MAIN MAIN MAIN //
+	
+	public static void main(String[] args)
+	{
+		System.out.println("");
+		
+	}
 }
